4e51b5b7247a385c41f6be394cb900a6e69229e2
Sberry5 <shannon.berryheller@gmail.com>
2017-10-06 21:20:34 -0500

    Computer selects works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,39 +11,38 @@ var losses = 0;
 //Variable to represent the remaining number of guesses a user has left
 var guessesLeft = 9;
 
-var computerSelects = "";
+var compLetter = "";
 
 //--------------------Reset Function---------------------
 var reset = function () {
-	wins = 0;
-	losses = 0;
-	guessesLeft = 9;
-	lettersGuessed = [];
+    wins = 0;
+    losses = 0;
+    guessesLeft = 9;
+    lettersGuessed = [];
 }
 
 //--------------------Game---------------------
-window.onload = function (){
-	computerSelects = computerLetter[Math.floor(Math.random() * computerLetter.length)];
-	}
-console.log(computerLetter);
+var comLetter = computerLetter[Math.floor(Math.random()*computerLetter.length)];
+console.log(comLetter);
 
-	document.onkeyup = function(event) {
-	var userGuess = event.key;
+
+    document.onkeyup = function(event) {
+    var userGuess = event.key;
 //Randomly select letter
 
 //If/else to determine
-    if ((userGuess === computerSelects)) {
-    	wins++;
-    	console.log(wins)
+    if ((userGuess === computerLetter)) {
+        wins++;
+        console.log(wins)
     }
-    else ((userGuess != computerSelects))
+    else ((userGuess != compLetter))
         losses++;
-    	lettersGuessed.push(userGuess)
-    	document.getElementById("guessed").innerHTML = "Your guesses so far: " + lettersGuessed;
-//    	console.log(lettersGuessed)
-    	};
+        lettersGuessed.push(userGuess)
+        document.getElementById("guessed").innerHTML = "Your guesses so far: " + lettersGuessed;
+//      console.log(lettersGuessed)
+        };
 //Variable to update page
-//	var pageText =
+//  var pageText =
 //          "<p>Wins: " + wins + "</p>" +
 //          "<p>Losses: " + losses + "</p>" +
 //          "<p>Guesses Left" + guessesLeft + "</p>" +
@@ -51,5 +50,3 @@ console.log(computerLetter);
 //        document.querySelector("#game").innerHTML = html;
 //    }
 //};
-
-//---------------------------------------------------
